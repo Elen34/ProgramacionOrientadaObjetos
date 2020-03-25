01bd81989f2583bc56ee40f068d8717e551db87b
Elen34 <lauelena_14@hotmail>
2020-03-25 10:18:08 -0600

    Primer Commit

---
+++ TipoParabolico/Javascript/App.js
@@ -0,0 +1,16 @@
+EventListener();
+
+function EventListener() {
+  document
+    .getElementById("btnCalcular")
+    .addEventListener("click", CalcularRecorrido);
+}
+
+function CalcularRecorrido() {
+  let angulo = Number(document.getElementById("angulo").value);
+  let velocidad = Number(document.getElementById("velocidad").value);
+  //Instanciar clase
+  const proyectil = new Proyectil(velocidad, angulo);
+
+  proyectil.crearSpan();
+}
